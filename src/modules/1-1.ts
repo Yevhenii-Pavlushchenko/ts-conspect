d438f5c4fd731424c46224674361896b4a3bda25
JustFun <paw93@ukr.net>
2026-02-26 07:47:41 +0200

    +++

---
+++ src/modules/1-1.ts
@@ -0,0 +1,203 @@
+// let age: number = "Jacob"; 
+// // ❌ Error: Type 'string' is not assignable to type 'number'
+
+// age = true; 
+// // ❌ Error: Type 'boolean' is not assignable to type 'number'
+// console.log(age);
+// example.ts
+
+// const message: string = "Hello, TypeScript!";
+// console.log(typeof(message));
+
+// let age = 26; // TypeScript виводить тип як number
+// console.log("🚀 ~ age:", age)
+
+// ❌ Error: Type 'string' is not assignable to type 'number'
+
+// let isOnline = false; // TypeScript виводить тип як boolean
+// console.log("🚀 ~ isOnline:", isOnline)
+
+// ❌ Error: Type 'number' is not assignable to type 'boolean'
+
+// const user: { name: string; age: number } = {
+//   name: "Alice"
+// };
+// console.log("🚀 ~ user:", user)
+// ❌ Error: Property 'age' is missing in type '{ name: string; }' 
+// but required in type '{ name: string; age: number; }'.
+
+// console.log(user.email); // Оголошуємо інтерфейс користувача
+// interface User {
+//   readonly id: number;
+//   name: string;
+//   age: number;
+//   isOnline?: boolean;
+// }
+
+// Використовуємо інтерфейс для типізації
+// const poly: User = {
+//   id: 4,
+//   name: "Poly",
+//   age: 25,
+//   isOnline: true
+// };
+
+// poly.id = 6;
+// poly.age =18
+// console.log(poly.id);
+
+
+// const jacob: User = {
+//   id:5,
+//   name: "Jacob",
+//   age: 36,
+//   isOnline: false
+// };
+
+// console.log(poly,jacob);
+
+// let res = []
+// res.push(poly,jacob)
+// console.log("🚀 ~ res:", res)
+
+
+// const numbers = [1, 2, 3]; 
+// console.log("🚀 ~ numbers:",typeof( numbers))
+
+
+// const names = ["Alice", "Bob"]; 
+// console.log("🚀 ~ names:", typeof(names[0]),typeof(names[1]),typeof(names[2]))
+
+// const numbers: number[] = [1, 2, 3];
+// console.log("🚀 ~ numbers:", numbers)
+
+// numbers.push(4); // ✅ Можна додати число
+// console.log("🚀 ~ numbers:", numbers)
+
+// numbers.push("5"); 
+// console.log("🚀 ~ numbers:", numbers)
+
+// const names: string[] = ["Alice", "Bob"];
+
+// console.log(names[0].toUpperCase()); 
+// ❌ Error: Object is possibly 'undefined'.
+
+// interface User {
+//   name: string;
+//   age: number;
+// }
+
+// const users: User[] = [
+//   { name: "Alice", age: 25 },
+//   { name: "Jacob", age: 30 }
+// ];
+// console.log("🚀 ~ users:", users)
+
+// users.push({ name: "Charlie", age: 18}); 
+// console.log("🚀 ~ users:", users)
+
+// const numbers: number[] = [1, 2, 3, 4, 5];
+// console.log(numbers.length);
+// const doubledNumbers = numbers.map(num => num * 2); 
+// console.log(doubledNumbers); // [2, 4, 6, 8, 10]
+
+// let userId: string | number = 123; // ✅
+// userId = "user-456"; // ✅
+// console.log("🚀 ~ userId:", userId)
+// userId = false; // ❌
+
+// Базова структура HTTP-відповіді
+// interface HttpResponse {
+//   status: number;
+//   message: string;
+// }
+
+// Специфічна відповідь для користувача
+// interface UserData {
+//   id: number;
+//   name: string;
+//   email?: string;
+// }
+
+// Поєднання базової відповіді з даними користувача
+// type UserResponse = HttpResponse & { data: UserData };
+
+// const response: UserResponse = {
+//   status: 200,
+//   message: "asdadsdasdsad",
+//   data: {
+//     id: 1,
+//     name: "Alice",
+//     email:"@sdasdsdad"
+//   }
+  // status: 200,
+  // message: "Success",
+  // data: {
+  //   id: 1,
+  //   name: "Alice",
+  //   email: "alice@example.com"
+  // }
+// };
+// console.log("🚀 ~ response:", response)
+
+// console.log(response.data.name); // Alice
+
+// function greet(name: string, age: number): void {
+//   console.log(`Hello, my name is ${name} and I am ${age} years old.`);
+// }
+// greet("Alice", 30); // ✅
+// greet(25, "Alice"); 
+// ❌ Error: Argument of type 'number' is not assignable to parameter of type 'string'.
+
+
+// function sum(a: number, b: number): number {
+  // return a + b;
+// }
+// const result = sum(5, 10); // ✅ result матиме тип number
+// console.log("🚀 ~ result:", result)
+
+//? Розглянемо задачу, де необхідно написати функцію, що отримує список користувачів
+//? та повертає імена цих користувачів у вигляді масиву рядків. Ось як ми можемо 
+//?типізувати таку функцію:
+
+// interface User{
+//   id: number
+//   name: string
+// }
+
+// const getUserNames = (users: User[]): string[] => {
+//   return users.map((user)=>user.name)
+// } 
+
+// const getUserId = (users: User[]): Number[] => {
+//   return users.map((user)=> user.id)
+// } 
+
+// const userList: User[] = [
+//   { id: 1, name: 'Alice' },
+//   { id: 2, name: 'Bob' },
+//     {id: 3 ,name: 'Charlei'},
+// ] 
+
+// const resName = getUserNames(userList)
+// console.log("🚀 ~ resName:", resName)
+// const resId = getUserId(userList)
+// console.log("🚀 ~ resId:", resId)
+
+// function greet(name: string, age?: number) {
+//   if (age !== undefined) {
+//     console.log(`Hello, my name is ${name} and I am ${age} years old.`);
+//   } else {
+//     console.log(`Hello, my name is ${name}.`);
+    
+//   }
+// }
+
+// greet('alice', 13);
+// greet("jonhy");
+
+//!Function Type (Тип функції)
+
+type Addfunctions = (a: number, b: number) => number;//задаем тип функции 
+const add: Addfunctions = (x, y) => x + y;// создаем функцию add и присваиваем ей тип !
+console.log( add(2, 5 ))
---
+++ src/modules/1-2.ts
@@ -0,0 +1,92 @@
+//!Generics у структурах даних
+
+import axios from "axios";
+
+// interface List < T > {
+//   items: T[];
+//   getItem: (index: number) => T;
+// }
+
+// const numberList: List<number> = {
+//   items: [1, 2, 3],
+//   getItem(index) {
+//     return this.items[index];
+//   },
+// };
+// console.log("🚀 ~ numberList:", numberList.getItem(0))
+
+// const stringList: List<string> = {
+//   items: ['Alice', 'Bob'],
+//   getItem(index) {
+//     return this.items[index];
+//   },
+// };
+// console.log("🚀 ~ stringList:", stringList.getItem(0))
+
+// function getLastItem<T>(arr: T[]): T {
+//  return arr[arr.length - 1];
+// }
+
+// const lastColor = getLastItem(["red", "green", "blue"]);
+// console.log("🚀 ~ lastColor:", typeof (lastColor))
+
+
+//!Типізація промісів
+const getData = (): Promise<string> => {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve("Hello, TypeScript!"), 3000);
+  });
+};
+
+getData().then((result) => console.log(result)); // "Hello, TypeScript!"
+
+// interface User {
+//   id: number;
+//   name: string;
+// }
+
+// const getUser = (): Promise<User> => {
+//   return new Promise((resolve) => {
+//     setTimeout(() => resolve({ id: 1, name: "Alice" }), 1000);
+//   });
+// }
+
+// getUser().then((user) => console.log(user.name)); // "Alice"
+// //Тут Promise<User> означає, що Promise виконається з об'єктом типу User.
+
+// //І, звичайно, якщо потрібно типізувати проміс із масивом:
+// const getUsers = (): Promise<User[]> => {
+//   return new Promise((resolve) => {
+//     setTimeout(() => resolve([
+//       { id: 1, name: "Alice" },
+//       { id: 2, name: "Bob" }
+//     ]), 1000);
+//   });
+// };
+
+// getUsers().then((users) => console.log(users[0].name)); // "Alice"
+
+//!Типізація fetch
+interface User {
+  id: number;
+  name: string;
+  email: string;
+}
+
+const fetchUsers = async (): Promise<User[]> => {
+  const response = await fetch("https://api.example.com/users");
+  const data = await response.json() as User[];
+  return data;
+};
+// console.log("🚀 ~ fetchUsers ~ fetchUsers:", fetchUsers)
+
+
+//!Типізація GETзапитів з Axios
+const fetchUser = async (userId: number): Promise<User> => {
+  const response = await axios.get<User>(
+	  `https://api.example.com/users/${userId}`
+	);
+  return response.data;
+};
+// console.log("🚀 ~ fetchUser ~ fetchUser:", fetchUser)
+
